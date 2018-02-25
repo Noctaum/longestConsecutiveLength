bd41e3a2270f55ee4dbaa113ac6a336d3d2e5636
Noctaum <Noctaum@users.noreply.github.com>
2018-02-25 16:48:38 +0300

    Task longestMass answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
+  let newArr = array.sort((a,b) => a-b);
+  let newArrNoReiter = [];
+  newArrNoReiter.push(newArr[0]);
+   for(let i = 0; i<newArr.length+1; i++){
+  	if(newArr[i] != (newArr[i+1])) newArrNoReiter.push(newArr[i+1]);
+  }
+  let count = 1, resultCount = 0;
+  for(let i = 1; i<newArrNoReiter.length; i++){
+  	if(newArrNoReiter[i] === (newArrNoReiter[i-1]+1)) count++;
+  	else if(newArrNoReiter[i] === (newArrNoReiter[i-1]));
+  	else{
+  		if(count > resultCount) resultCount = count;
+  		count = 1;
+  	}
+  }
+  return resultCount;
+
 }
+
